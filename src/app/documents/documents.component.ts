1dbc5bca3946c24a3cfa4d410f7793225fc35180
erickpati10 <pat21018@byui.edu>
2025-05-28 23:39:00 -0600

    Implement the Documents Feature

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Document } from './document.model';
 
 @Component({
   selector: 'cms-documents',
   standalone: false,
   templateUrl: './documents.component.html',
-  styleUrl: './documents.component.css'
+  styleUrl: './documents.component.css',
 })
 export class DocumentsComponent {
+  selectedDocument: Document | null = null;
 
+  constructor() {}
+
+  onDocumentSelected(document: Document) {
+    this.selectedDocument = document;
+  }
 }
